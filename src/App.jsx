23ea21669431243000cafbfb24e152ae299f8714
uniqudev <158754682+uniqudev@users.noreply.github.com>
2024-09-06 23:48:29 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import About from './pages/about';
 import SignIn from './pages/SingIn';
 import SignUp from './pages/SingUp';
 import Profile from './pages/Profile';
+import Header from "./components/header";
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />  
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/SingIn" element={<SignIn />} />
