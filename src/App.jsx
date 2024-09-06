750b14d9c0cdc9c8071b5a93f362560d0000c49e
uniqudev <158754682+uniqudev@users.noreply.github.com>
2024-09-06 22:12:35 +0530

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import About from './pages/about';
+import SignIn from './pages/SingIn';
+import SignUp from './pages/SingUp';
+import Profile from './pages/Profile';
 
 export default function App() {
-  return <h1 className='text-red-700'>app</h1>
-    
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/SingIn" element={<SignIn />} />
+        <Route path="/SingUp" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/Profile" element={<Profile />} /> 
+      </Routes>
+    </BrowserRouter>
+  );
 }
