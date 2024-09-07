547d724a69ec2a7567dbaf88b896d8933a1cf244
uniqudev <158754682+uniqudev@users.noreply.github.com>
2024-09-08 01:35:13 +0530

    create a middleware and a function to handle possible errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,18 +1,20 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const SingUp = async (req, res) => {
+export const SingUp = async (req, res,next) => {
 
     const {username,email,password} = req.body;
     const hashedPassword = await bcryptjs.hash(password,10);
     const newUser = new User({username,email,password:hashedPassword});
+    
     try{
         
         await newUser.save()
         res.status(201).json('User created successfully!!');
     }catch (error){
-        res.status(500).json({message:error.message})
-    }    
+        next(error);
+    };    
     
     
 };
