a6430b40a0b643c4c0493d30af80901568565b4c
uniqudev <158754682+uniqudev@users.noreply.github.com>
2024-09-08 14:40:49 +0530

    create signin in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const SingUp = async (req, res,next) => {
 
@@ -15,6 +16,24 @@ export const SingUp = async (req, res,next) => {
     }catch (error){
         next(error);    
     };    
-    
-    
+     
 };
+
+export const SingIn = async (req,res,next) =>{
+    const {email,password} = req.body;
+
+    try {
+        const validUser = await User.findOne({email});
+        if (!validUser) return next(errorHandler(404,'User not Found!'));
+        const validPassword = bcryptjs.compareSync(password,validUser.password);
+        if (!validPassword) return next(errorHandler(401,'Wrong cridentials!'));
+        const token = jwt.sign({ id:validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest} = validUser._doc;
+        res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+    } catch (error) {
+        next(error);
+    }
+}
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { SingUp } from '../controllers/auth.controller.js';
+import { SingUp,SingIn } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 router.post("/SingUp",SingUp);
+router.post("/SingIn",SingIn);
 
 export default router;
